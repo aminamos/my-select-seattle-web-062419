87239f55ea8b8828a020180129280808610bf6ac
Amin Amos <aminamosahs@gmail.com>
2019-06-20 16:55:07 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,16 @@
-def my_select(criteria)
-  array = [] # establish array to return at the end
-  if criteria.length == 0
+def my_select(array)
+  narray = [] # establish array to return at the end
+  if array.length == 0
     "nothing here" # can handle no criteria given
   else
-    criteria.each {|x| 
+    array.each {|x| 
       if x == arg # this is where I'm confused
         array << x 
       end
     }
   end
+  
   array
+
 end
 
