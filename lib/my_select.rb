489b590a1296eaa21494c70a5b05e4636860ccee
Amin Amos <aminamosahs@gmail.com>
2019-06-20 16:59:48 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,19 +1,14 @@
-require "pry"
-
 def my_select(array)
   narray = [] # establish array to return at the end
   if array.length == 0
     "nothing here" # can handle no criteria given
   else
     array.each {|x| 
-    binding.pry
-      if x # ==  arg this is where I'm confused
+    
+      if yield(x) == true
         narray << x 
       end
     }
   end
-  
   narray
-
 end
-
