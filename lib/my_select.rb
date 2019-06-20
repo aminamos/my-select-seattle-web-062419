061415f89a04d0aae74e544f02a7033976987007
Amin Amos <aminamosahs@gmail.com>
2019-06-20 16:26:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,24 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
+
+def my_select(collection)
+  array = []
+  if collection.length == 0
+    "nothing here"
+  else
+    collection.each {|x|
+      if x
+    }
+  
 end
